d672ad282850b1fce084ecfc99a2baf8e6a80161
palomacadastra <paloma.santos@cadastra.com>
2024-12-21 15:04:14 -0300

    feat(newsletter): cria newsletter desktop

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -1,3 +1,18 @@
+import "./styles.scss";
+
 export const Newsletter = () => {
-  return <div></div>;
+  return (
+    <div className="newsletter-container">
+      <div className="newsletter-container__wrapper">
+        <h2 className="newsletter-container__title">
+          Participe de nossas news com promoções e novidades!
+        </h2>
+        <form className="newsletter-container__form">
+          <input type="text" placeholder="Digite seu nome" />
+          <input type="text" placeholder="Digite seu email" />
+          <button type="submit">Eu quero!</button>
+        </form>
+      </div>
+    </div>
+  );
 };
