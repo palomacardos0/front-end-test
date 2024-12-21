3fae3d15d80fd522ce8aa2a17835921d6a8710a9
palomacadastra <paloma.santos@cadastra.com>
2024-12-21 13:47:16 -0300

    feat(footer): cria footer descktop

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,38 @@
+import { Newsletter } from "../Newsletter";
+import Email from "../../assets/email.svg";
+import Headset from "../../assets/headset.svg";
+import Logo from "../../assets/logo-white.svg";
+
+import "./styles.scss";
+
+export const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <Newsletter />
+      <div className="footer-container__wrapper">
+        <div className="footer-container__top">
+          <a className="footer-container__buttons">
+            <img src={Email} alt="Icon de Email" />
+            ENTRE EM CONTATO
+          </a>
+          <a>
+            <img src={Headset} alt="Icone de fone" />
+            FALE COM O NOSSO CONSULTOR ONLINE
+          </a>
+        </div>
+        <div className="footer-container__bottom">
+          <div className="footer-container__location">
+            <h3>Localização</h3>
+            <span className="footer-container__separator"></span>
+            <p>Avenida Andrômeda, 2000. Bloco 6 e 8 - Alphavile SP</p>
+            <p>brasil@corebiz.ag</p>
+            <p>+55 11 3090 1039</p>
+          </div>
+          <div>
+            <span>Created by</span> <img src={Logo}></img>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
---
+++ src/components/Newsletter/index.tsx
@@ -0,0 +1,3 @@
+export const Newsletter = () => {
+  return <div></div>;
+};
