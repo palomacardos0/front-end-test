6ea0cf28fc2af5c190304504462d372285de90a6
palomacadastra <paloma.santos@cadastra.com>
2024-12-21 10:24:32 -0300

    feat(header): cria header desktop

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,33 @@
 import Logo from "../../assets/logo.svg";
+import Search from "../../assets/search.svg";
+import User from "../../assets/user.svg";
+import Cart from "../../assets/cart.svg";
+import "./styles.scss";
 
 export const Header = () => {
   return (
-    <header>
-      <div>
-        <img src={Logo} alt="Logo Corebiz" />
-        <div></div>
-        <div></div>
+    <header className="header-container">
+      <div className="header-container__wrapper">
+        <a href="/">
+          <img src={Logo} alt="Logo Corebiz" />
+        </a>
+        <div className="header-container__search">
+          <input type="text" />
+          <button>
+            <img src={Search} alt="Buscar" />
+          </button>
+        </div>
+
+        <div className="header-container__cart">
+          <a href="/account">
+            <img src={User} alt="Ícone Usuário" />
+            <span>Minha Conta</span>
+          </a>
+          <button>
+            <img src={Cart} alt="Carrinho" />
+            <span>1</span>
+          </button>
+        </div>
       </div>
     </header>
   );
