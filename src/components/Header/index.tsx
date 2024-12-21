52efaccd0a7be97d25fe97db8499407a9f61e30a
palomacadastra <paloma.santos@cadastra.com>
2024-12-21 11:25:13 -0300

    feat(header):cria header mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,63 @@
+import { useMediaQuery } from "react-responsive";
+
 import Logo from "../../assets/logo.svg";
 import Search from "../../assets/search.svg";
 import User from "../../assets/user.svg";
 import Cart from "../../assets/cart.svg";
+import Menu from "../../assets/menu.svg";
 import "./styles.scss";
 
 export const Header = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
   return (
     <header className="header-container">
-      <div className="header-container__wrapper">
-        <a href="/">
-          <img src={Logo} alt="Logo Corebiz" />
-        </a>
-        <div className="header-container__search">
-          <input type="text" />
-          <button>
-            <img src={Search} alt="Buscar" />
-          </button>
+      {isMobile ? (
+        <div className="header-container__wrapper">
+          <div className="header-container__top">
+            <button>
+              <img src={Menu} alt="Menu" />
+            </button>
+            <a href="/" className="header-container__logo">
+              <img src={Logo} alt="Logo Corebiz" />
+            </a>
+            <div className="header-container__cart">
+              <button>
+                <img src={Cart} alt="Carrinho" />
+                <span>1</span>
+              </button>
+            </div>
+          </div>
+          <div className="header-container__search">
+            <input type="text" placeholder="O que está procurando?" />
+            <button>
+              <img src={Search} alt="Buscar" />
+            </button>
+          </div>
         </div>
-
-        <div className="header-container__cart">
-          <a href="/account">
-            <img src={User} alt="Ícone Usuário" />
-            <span>Minha Conta</span>
+      ) : (
+        <div className="header-container__wrapper">
+          <a href="/">
+            <img src={Logo} alt="Logo Corebiz" />
           </a>
-          <button>
-            <img src={Cart} alt="Carrinho" />
-            <span>1</span>
-          </button>
+          <div className="header-container__search">
+            <input type="text" placeholder="O que está procurando?" />
+            <button>
+              <img src={Search} alt="Buscar" />
+            </button>
+          </div>
+
+          <div className="header-container__cart">
+            <a href="/account">
+              <img src={User} alt="Ícone Usuário" />
+              <span>Minha Conta</span>
+            </a>
+            <button>
+              <img src={Cart} alt="Carrinho" />
+              <span>1</span>
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </header>
   );
 };
